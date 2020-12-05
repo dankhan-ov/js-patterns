6860d14527a6b4fdd900f5ff59d536be2d44bde2
khan <daniilhanchenkov@yandex.ru>
2020-12-05 22:20:19 +0300

    add abstract factory

---
+++ abstractFactory.js
@@ -0,0 +1,34 @@
+// Abstract Factory - паттерн, создающий интерфейс, группирующий другие фабрики, 
+// логически связанных друг с другом
+
+// Superclass
+function bmwProducer(kind) {
+  return kind === 'sport' ? sportCarFactory : familyCarFactory;
+}
+
+// Factories
+function sportCarFactory() {
+	return new Z4();
+}
+
+function familyCarFactory() {
+	return new I3();
+}
+
+class Z4 {
+	info() {
+		return "Z4 is a Sport car!";
+	}
+}
+
+class I3 {
+	info() {
+		return "i3 is a Family car!";
+	}
+}
+
+const produce = bmwProducer();
+
+const myCar = new produce();
+
+console.log(myCar.info());
