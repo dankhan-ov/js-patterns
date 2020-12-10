e748020bc363ba0c3936f9fe75f1087dc3fe6ce0
khan <daniilhanchenkov@yandex.ru>
2020-12-10 16:34:55 +0300

    add template

---
+++ templte-method.js
@@ -0,0 +1,53 @@
+// Template Method - поведенческий паттерн, определяющий базовые шаги исполнения алгоритма;
+// выполнение каждого шага делегирует на соответствующие методы и подклассы.
+
+class Builder {
+	build() {
+		this.addEngine();
+		this.installChassis();
+		this.addElectronic();
+		this.collectAccessories();
+	}
+};
+
+class TeslaBuilder extends Builder {
+	addEngine() {
+		console.log('Add Electronic Engine');
+	}
+
+	installChassis() {
+		console.log('Install Tesla chassis');
+	}
+
+	addElectronic() {
+		console.log('Add special electronic');
+	}
+
+	collectAccessories() {
+		console.log('Collect Accessories');
+	}
+};
+
+class BmwBuilder extends Builder {
+	addEngine() {
+		console.log('Add BMW Engine');
+	}
+
+	installChassis() {
+		console.log('Install BMW chassis');
+	}
+
+	addElectronic() {
+		console.log('Add electronic');
+	}
+
+	collectAccessories() {
+		console.log('Collect Accessories');
+	}
+};
+
+const teslaBuilder = new TeslaBuilder();
+const bmwBuilder = new bmwBuilder();
+
+teslaBuilder.build();
+bmwBuilder.build();
